a446832548f271a00e5e0d3ec03dd5c0e70303f8
Nice <108124042+NicetoMeets@users.noreply.github.com>
2023-03-30 17:48:37 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,8 +1,10 @@
 function solution(slice, n) {
     let piz=1;
    while(true){
-       if(piz*slice/n>=1) return piz
+       if(piz*slice-n>=0) 
+           return piz
        
        piz++
    }
 }
+
