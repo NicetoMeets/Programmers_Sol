0371d91e70974dfdd58a8b87a56719385bb3f5ba
Nice <108124042+NicetoMeets@users.noreply.github.com>
2023-03-31 17:11:26 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,23 +1,10 @@
 function solution(lines) {
-  var answer = 0;
-  let lineMap = new Array(200);  // 선분들이 놓일 공간
-  lineMap.fill(0);
 
+  let line = new Array(200).fill(0);
 
-  for (let i = 0; i < 3; i++) {
-    let left = lines[i][0];
-    let right = lines[i][1];
+  lines.forEach(([a, b]) => {
+      for(; a < b; a++) line[a+100]++;
+  });
 
-    for (let j = left; j < right; j++) {
-      lineMap[j + 100] += 1;
-    }
-  }
-
-  for (let i in lineMap) {
-    if (lineMap[i] > 1) {
-      answer += 1;
-    }
-  }
-
-  return answer;
+  return line.reduce((a, c) =>  c > 1 ? a + 1 : a, 0)
 }
