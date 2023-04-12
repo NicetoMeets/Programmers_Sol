2abf17d6104ade2b4a1b5c031613981daddce269
Nice <108124042+NicetoMeets@users.noreply.github.com>
2023-04-12 13:58:00 +0900

    [unrated] Title: 둘만의 암호, Time: 6.01 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -1,16 +1,22 @@
-function solution(s,skip,index) {
-  const strArr = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  const skipArr = skip.split(''); 
-  let ans = ''
-  const newStr = strArr.filter(e=> skipArr.indexOf(e) === -1)
-  const newStrLength = newStr.length;
-  s.split('').map(e=> {
-    let idx = newStr.indexOf(e)
-    idx += index
-    while(idx > newStrLength - 1){
-      idx -= newStrLength
-    }
-    ans += newStr[idx]
-  })
-  return ans
+function solution(s, skip, index) {
+
+    let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    
+    let arrS = [...s]
+    let arrSkip = [...skip]
+
+    console.log('arrS', arrS)
+    console.log('arrSkip', arrSkip)
+    
+    let alpha = alphabet.filter((el)=> !arrSkip.includes(el))
+
+    console.log('alpha', alpha)
+    
+    let ans = arrS.map((el)=>{
+      let num = alpha.indexOf(el)
+      let addNum = num + index
+      
+      return alpha[addNum%alpha.length]
+    })
+    return ans.join('')
 }
