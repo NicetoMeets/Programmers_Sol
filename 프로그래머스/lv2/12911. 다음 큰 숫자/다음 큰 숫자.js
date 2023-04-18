576c9625c11c28fd2ac8dc56f09d0c6cac50f07f
Nice <108124042+NicetoMeets@users.noreply.github.com>
2023-04-18 14:13:36 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  let a = n+1
+  let Ncount = 0;
+  
+  n.toString(2).split('').map((el)=>{
+      if(el=='1') Ncount++
+  })
+
+  while(true){
+      let Acount = 0;
+      a.toString(2).split('').map((el)=>{
+      if(el=='1') Acount++})
+          
+      if(Ncount==Acount) return a
+
+      a+=1
+  
+  }
+}
